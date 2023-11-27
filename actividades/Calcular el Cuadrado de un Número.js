591adb331d6a827fbb0613e12541f25d3ebb0a16
Alexgv0 <143190790+Alexgv0@users.noreply.github.com>
2023-11-27 20:48:40 -0300

    Delete actividades directory

--- actividades/Calcular el Cuadrado de un Número.js
+++
@@ -1,7 +0,0 @@
-//Calcular el Cuadrado de un Número
-
-let num1 = Number(prompt("Introduce un numero: "));
-
-alert("El cuadrado del numero " + num1 + " es: " + (num1*num1));
-
-location.reload()
--- actividades/Calcular el Descuento de un Producto.js
+++
@@ -1,8 +0,0 @@
-//Calcular el Descuento de un Producto
-
-let num1 = parseFloat(prompt("Ingrese el valor del producto:"));
-let num2 = parseFloat(prompt("Ingrese el porcentaje de descuento:"));
-
-alert("El descuento es de " + ((num1*num2)/100));
-
-location.reload()
